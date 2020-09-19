c23f8ab8ddfd991645b512f2512f2fb84bbbac35
rivendor <flimarques26@gmail.com>
2020-09-19 13:57:20 -0300

    Api fetch sucess. Propely formating data and showing movie cards. Start working on pagination.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,100 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+// Axios
+import axios from 'axios'
+
+async function getPopularList (setMovieList) {
+    const moviedb = axios.create({baseURL: 'https://api.themoviedb.org/3/'})
+    const apiKey = '?api_key=0e1e1173c64373fe6b05e8aa643359ad'
+
+    // Get popular movies
+     await moviedb.get(`/movie/popular${apiKey}&language=pt-BR`)
+      .then(res => {
+
+        console.log(res.data.results[0].original_title)
+
+        setMovieList(res.data.results)
+      })
+      .catch(e => {
+        console.error(e)
+      })
+}
+
+async function getGenreList (setGenreList) {
+  const moviedb = axios.create({baseURL: 'https://api.themoviedb.org/3/'})
+  const apiKey = '?api_key=0e1e1173c64373fe6b05e8aa643359ad'
+
+  // Get popular movies
+   await moviedb.get(`/genre/movie/list${apiKey}&language=pt-BR`)
+    .then(res => {
+
+      console.log(res.data.genres)
+
+      setGenreList(res.data.genres)
+    })
+    .catch(e => {
+      console.error(e)
+    })
+}
+
+function getGenreName (genreId, genreList) {
+  const genreName = genreList.map((genre, index) => {
+    if (genreId === genre.id) return genre.name
+  })
+  return genreName
+}
+
+function mapMovies (movieList, genreList) {
+  return movieList.map((movie, index) => {
+    if (!movie) {return <p>Loading</p>}
+    else {
+      // Date format
+      const date = movie.release_date.substr(8, 2) +
+      '/' + movie.release_date.substr(5, 2) +
+      '/' + movie.release_date.substr(0, 4)
+
+      // Vote format
+      const vote = movie.vote_average.toString().replace('.', '')
+
+      return <div key={index} className="Movies-card-container">
+        <img className="Movies-card-image"
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.original_title} />
+        <div className="Movies-card-text-container">
+          <div className="Movies-card-percentage-circle">
+            <h2 className="Movies-card-percentage">{vote}%</h2>
+          </div>
+          <div className="Movies-card-header-container">
+          <h2 className="Movies-card-header-container-title">{movie.title}</h2>
+          </div>
+          <p className="Movies-card-date">{date}</p>
+          <p className="Movies-card-overview">{movie.overview}</p>
+          <div className="Movies-card-tag-row">
+            {
+              // Genre names
+                movie.genre_ids.map((id, index) => {
+                  const name = getGenreName(id, genreList)
+                  return <a key={index} className="Movies-card-tag" href="#">{name}</a>
+                })
+            }
+          </div>
+        </div>
+      </div>
+    }
+  })
+}
+
 function App() {
+  const [movieList, setMovieList] = useState([])
+  const [genreList, setGenreList] = useState([])
+
+  useEffect(() => {
+    getPopularList(setMovieList)
+    getGenreList(setGenreList)
+    return console.log('Unmounted')
+  }, [])
+
   return (
     <div className="flex-container">
       <header className="Movies-header">
@@ -13,75 +106,9 @@ function App() {
             <input type="text" placeholder="Busque um filme por nome, ano ou gênero..." />
           </div>
           <div className="Movies-card-list">
-            <div className="Movies-card-container">
-              <img className="Movies-card-image"
-                src="https://upload.wikimedia.org/wikipedia/pt/7/7d/Thor_Ragnarok_poster.jpg"
-                alt="Thor: Ragnarok" />
-              <div>
-                <div className="Movies-card-percentage-circle">
-                  <h2 className="Movies-card-percentage">75%</h2>
-                </div>
-                <div className="Movies-card-header-container">
-                  <h2 className="Movies-card-header-container-title">Thor: Ragnarok</h2>
-                </div>
-                <p className="Movies-card-date">25/10/2017</p>
-                <p className="Movies-card-overview">A ticking-time-bomb insomniac and a slippery soap salesman channel primal male
-                  aggression into a shocking new form of therapy. Their concept catches on, with underground
-                    \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an
-                    out-of-control spiral toward oblivion.</p>
-                <div className="Movies-card-tag-row">
-                  <a className="Movies-card-tag" href="#">Ação</a>
-                  <a className="Movies-card-tag" href="#">Aventura</a>
-                  <a className="Movies-card-tag" href="#">Fantasia</a>
-                </div>
-              </div>
-            </div>
-            <div className="Movies-card-container">
-              <img className="Movies-card-image"
-                src="https://upload.wikimedia.org/wikipedia/pt/7/7d/Thor_Ragnarok_poster.jpg"
-                alt="Thor: Ragnarok" />
-              <div>
-                <div className="Movies-card-percentage-circle">
-                  <h2 className="Movies-card-percentage">75%</h2>
-                </div>
-                <div className="Movies-card-header-container">
-                  <h2 className="Movies-card-header-container-title">Thor: Ragnarok</h2>
-                </div>
-                <p className="Movies-card-date">25/10/2017</p>
-                <p className="Movies-card-overview">A ticking-time-bomb insomniac and a slippery soap salesman channel primal male
-                  aggression into a shocking new form of therapy. Their concept catches on, with underground
-                    \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an
-                    out-of-control spiral toward oblivion.</p>
-                <div className="Movies-card-tag-row">
-                  <a className="Movies-card-tag" href="#">Ação</a>
-                  <a className="Movies-card-tag" href="#">Aventura</a>
-                  <a className="Movies-card-tag" href="#">Fantasia</a>
-                </div>
-              </div>
-            </div>
-            <div className="Movies-card-container">
-              <img className="Movies-card-image"
-                src="https://upload.wikimedia.org/wikipedia/pt/7/7d/Thor_Ragnarok_poster.jpg"
-                alt="Thor: Ragnarok" />
-              <div>
-                <div className="Movies-card-percentage-circle">
-                  <h2 className="Movies-card-percentage">75%</h2>
-                </div>
-                <div className="Movies-card-header-container">
-                  <h2 className="Movies-card-header-container-title">Thor: Ragnarok</h2>
-                </div>
-                <p className="Movies-card-date">25/10/2017</p>
-                <p className="Movies-card-overview">A ticking-time-bomb insomniac and a slippery soap salesman channel primal male
-                  aggression into a shocking new form of therapy. Their concept catches on, with underground
-                    \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an
-                    out-of-control spiral toward oblivion.</p>
-                <div className="Movies-card-tag-row">
-                  <a className="Movies-card-tag" href="#">Ação</a>
-                  <a className="Movies-card-tag" href="#">Aventura</a>
-                  <a className="Movies-card-tag" href="#">Fantasia</a>
-                </div>
-              </div>
-            </div>
+            {
+              mapMovies(movieList, genreList)
+            }
           </div>
         </main>
       </body>
